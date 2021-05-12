52ec2f7efdf3ef54c0f765f8163c54c6b327edad
Christiano0407 <placebo0704@gmail.com>
2021-05-12 12:52:16 -0500

    previous-One

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,46 @@ TweenMax.from(".left-col", 2, {
     width: "0%", 
     ease: Expo.easeInOut
 })
+
+TweenMax.from(".logo", 2, {
+    delay: 2.5, 
+    y: 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
+TweenMax.from(".buy", 2, {
+    delay: 2.8,
+    y: 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
+TweenMax.from(".year", 2, {
+    delay: 3.1, 
+    y: 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
+TweenMax.from(".album-name", 2, {
+    delay: 2.5, 
+    y: 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
+TweenMax.from(".album-artist", 2, {
+    delay: 3, 
+    y: 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
+TweenMax.from(".about h1", 2, {
+    delay: 3, 
+    y : 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
+TweenMax.from(".album-name2", 2, {
+    delay: 3.2, 
+    y: 20, 
+    opacity: 0, 
+    ease: Expo.easeInOut
+})
